ee3dc174af4f2ce88394196fb5a079b4ada95a51
Nika Lobanova <137531398+vagabondry@users.noreply.github.com>
2024-04-23 00:31:25 +0300

    Sanity System

---
+++ Assets/scripts/Sanity Manager.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering.PostProcessing;
+
+public class SanityManager : MonoBehaviour
+{
+    public Slider sanitySlider;
+    public int fullSanity = 100;
+    public float lightRecoveryRate = 5f;
+    public float darkDecayRate = 10f;
+    public float enemyNearbyAdditionalDecay = 5f;
+    public float enemyDetectionRadius = 10f;
+    public PostProcessProfile postProcessProfile;
+    public GameObject player;
+
+    private Vignette vignette;
+    private bool isPlayerInLight = false;
+    private bool isGamePaused = false;
+
+    void Start()
+    {
+        postProcessProfile.TryGetSettings(out vignette);
+        sanitySlider.maxValue = fullSanity;
+        sanitySlider.value = fullSanity;
+        vignette.intensity.value = 0;
+
+        StartCoroutine(LoseSanity());
+    }
+
+    IEnumerator LoseSanity()
+    {
+        while (sanitySlider.value > 0)
+        {
+            if (isPlayerInLight)
+            {
+                sanitySlider.value = Mathf.Min(fullSanity, sanitySlider.value + lightRecoveryRate * Time.deltaTime);
+            }
+            else
+            {
+                float decayRate = darkDecayRate + (IsEnemyNearby() ? enemyNearbyAdditionalDecay : 0);
+                sanitySlider.value = Mathf.Max(0, sanitySlider.value - decayRate * Time.deltaTime);
+             }
+
+             UpdateVignetteEffect();
+             yield return null;
+        }
+
+        Debug.Log("Player has gone insane.");
+    }
+
+    void UpdateVignetteEffect()
+    {
+        float newValue = (sanitySlider.value - sanitySlider.maxValue) * (-1);
+        float percent = newValue / sanitySlider.maxValue;
+        vignette.intensity.value = percent;
+    }
+
+    bool IsEnemyNearby()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(player.transform.position, enemyDetectionRadius);
+        foreach (var hitCollider in hitColliders)
+        {
+            if (hitCollider.gameObject.CompareTag("Enemy"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("LightSource"))
+        {
+            isPlayerInLight = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("LightSource"))
+        {
+            isPlayerInLight = false;
+        }
+    }
+
+    public void ToggleGamePause(bool paused)
+    {
+        isGamePaused = paused;
+    }
+}
